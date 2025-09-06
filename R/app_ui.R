98d319d4205022151d8560eec2c11438b74157ea
tdelcey <thomas.delcey@ube.fr>
2025-09-06 17:06:53 +0200

    update ui

--- R/app_ui.R
+++ R/app_ui.R
@@ -13,15 +13,14 @@ app_ui <- function(request) {
       # NEW TAB
       tabPanel(
         "Textual clusters",
-        # fill viewport height under the navbar; tweak the 120px if needed
-        div(
-          style = "height: calc(100vh - 120px);",
-          tags$iframe(
-            src = "data/textual_cluster.html",   # file at project/data/textual_cluster.html
-            style = "width:100%; height:100%; border:0;",
-            loading = "lazy"
-          )
-        )
+        div(style = "height: calc(100vh - 120px);",
+            tags$iframe(
+              src   = "bightml/textual_cluster.html",
+              style = "width:100%; height:100%; border:0;",
+              loading = "lazy"
+            )
+        ),
+        tags$p(tags$a(href = "bightml/textual_cluster.html", target = "_blank", "Open full screen"))
       )
     )
   )
