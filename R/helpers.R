3bf89415ab99a444731fc335aff49a88aea2f2e1
tdelcey <thomas.delcey@ube.fr>
2025-12-01 17:29:45 +0100

    initial commit app V 2.0

--- R/helpers.R
+++
@@ -1,9 +0,0 @@
-# Helpers pour alléger le code
-simple_datatable <- function(df, pageLength = 5, escape = TRUE) {
-  DT::datatable(
-    df,
-    options = list(pageLength = pageLength),
-    rownames = FALSE,
-    escape = escape
-  )
-}
